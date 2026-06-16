9888ea3e0534d897186bebce7ffd1d18d927270d
obaidullah47 <bsf23004001@ue.edu.pk>
2026-06-17 00:04:49 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/home_screen.dart';
+import 'package:myapp/obaidullah.dart';
+import 'package:myapp/wasiullah.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.id,
+      routes: {HomeScreen.id: (context) => HomeScreen(),
+        obaidullah.id:(context)=>obaidullah(),
+        wasiullah.id:(context)=>wasiullah(),
+      },
+
+    );
+  }
+}
