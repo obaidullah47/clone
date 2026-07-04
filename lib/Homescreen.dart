613bd168e18b9a410ae8aa13f888995b60649f68
obaidullah47 <bsf23004001@ue.edu.pk>
2026-07-04 15:49:25 +0500

    complete calculator

--- lib/Homescreen.dart
+++ lib/Homescreen.dart
@@ -1,5 +1,6 @@
 import 'package:calculator/components/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 import 'constants.dart';
 
@@ -12,27 +13,233 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  var userInput = "";
+  var answer = "";
+  void equalpress() {
+    String finaluserInput= userInput.replaceAll("X", "*");
+    Parser p = Parser();
+    Expression expression = p.parse(finaluserInput);
+    ContextModel contextModel = ContextModel();
+    double eval = expression.evaluate(EvaluationType.REAL, ContextModel());
+    answer = eval.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
           children: [
-            button(
-              name: "LOGIN",
-              onPress: () {
-                print("click me");
-              },
-              color: Colors.greenAccent,
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        userInput.toString(),
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    Text(
+                      answer.toString(),
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            button(
-              name: "SIGN UP",
-              onPress: () {
-                print("Fuck u");
-              },
-              color: Colors.pinkAccent,
+
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Button(
+                        title: "AC",
+                        onPress: () {
+                          userInput = '';
+                          answer = "";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "+/-",
+                        onPress: () {
+                          userInput += "+/-";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "%",
+                        onPress: () {
+                          userInput += "%";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "/",
+                        color: Color(0xffFF9F0A),
+                        onPress: () {
+                          userInput += "/";
+                          setState(() {});
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+
+                    children: [
+                      Button(
+                        title: "7",
+                        onPress: () {
+                          userInput += "7";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "8",
+                        onPress: () {
+                          userInput += "8";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "9",
+                        onPress: () {
+                          userInput += "9";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "X",
+                        color: Color(0xffFF9F0A),
+                        onPress: () {
+                          userInput += 'X';
+                          setState(() {});
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+
+                    children: [
+                      Button(
+                        title: "4",
+                        onPress: () {
+                          userInput += "4";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "5",
+                        onPress: () {
+                          userInput += "5";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "6",
+                        onPress: () {
+                          userInput += "6";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "-",
+                        color: Color(0xFFFF9F0A),
+                        onPress: () {
+                          userInput += "-";
+                          setState(() {});
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+
+                    children: [
+                      Button(
+                        title: "1",
+                        onPress: () {
+                          userInput += "1";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "2",
+                        onPress: () {
+                          userInput += "2";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "3",
+                        onPress: () {
+                          userInput += "3";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "+",
+                        color: Color(0xffFF9F0A),
+                        onPress: () {
+                          userInput += "+";
+                          setState(() {});
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+
+                    children: [
+                      Button(
+                        title: "0",
+                        onPress: () {
+                          userInput += "0";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: ".",
+                        onPress: () {
+                          userInput += ".";
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "DEL",
+                        onPress: () {
+                          userInput = userInput.substring(
+                            0,
+                            userInput.length - 1,
+                          );
+                          setState(() {});
+                        },
+                      ),
+                      Button(
+                        title: "=",
+                        color: Color(0xffFF9F0A),
+                        onPress: () {
+                          equalpress();
+                          setState(() {});
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            button(name: "Continue", onPress: (){print("yeeaahhh buddyy");}, color: Color(0xff234323))
           ],
         ),
       ),
--- lib/components/buttons.dart
+++ lib/components/buttons.dart
@@ -1,47 +1,35 @@
 import 'package:flutter/material.dart';
 
-class button extends StatelessWidget {
-  final String name;
-  final VoidCallback onPress;
+class Button extends StatelessWidget {
+  final String title;
   final Color color;
-  const button({
+  final VoidCallback onPress;
+
+  const Button({
     super.key,
-    required this.name,
+    required this.title,
+    this.color = const Color(0xffa5a5a5),
     required this.onPress,
-    required this.color,
   });
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: SizedBox(
-              width: double.infinity,
-              height: 55,
-              child: TextButton(
-                onPressed: onPress,
-                style: TextButton.styleFrom(
-                  backgroundColor: color,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-                child: Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white70,
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: InkWell(
+          onTap: onPress,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
